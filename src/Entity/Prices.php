966717f6ad027a600fde76d390352dc01dfe97bf
Pedro <pedro@primepass.club>
2018-11-08 16:20:52 -0200

    Creating migrations

---
+++ src/Entity/Prices.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PricesRepository")
+ */
+class Prices
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
